88b6da92bc4a4126ba0fb98ea550ce87771e4393
cubedro <contact@siteshop.ro>
2015-03-07 19:40:12 +0200

    added images

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '/public/images/favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
