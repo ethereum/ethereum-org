329b1b78907d28e0930c794a4aa2c16e6818b730
cubedro <contact@siteshop.ro>
2015-03-09 17:13:21 +0200

    added json getBlock

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var bodyParser = require('body-parser');
 // Init the app
 var app = express();
 
+var approvedBlock = {
+	number: 0,
+	hash: "0x",
+	timestamp: 0
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -14,36 +20,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Ethereum Frontier' });
+	res.render('index', { title: 'Ethereum Frontier' });
+});
+
+app.get('/getBlock', function(req, res) {
+	res.json(approvedBlock);
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 module.exports = app;
