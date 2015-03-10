8b14dfb1faae2a7c7c8de579346710ab8a196bf7
cubedro <contact@siteshop.ro>
2015-03-10 16:39:41 +0200

    added view/hide source

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
-hljs.initHighlightingOnLoad();
+(function(){
+	hljs.initHighlightingOnLoad();
+
+	$('#shell-source').hide().removeClass('hidden');
+
+	$('#show-source').on('click', function() {
+		$('#shell-source').show();
+		$(this).hide();
+	});
+
+	$('#hide-source').on('click', function() {
+		$('#show-source').show();
+		$('#shell-source').hide();
+	});
+
+})();
+
