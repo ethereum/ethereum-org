580781d5059beec0f2852155e1925ed4fb91a182
cubedro <contact@siteshop.ro>
2015-03-20 16:22:29 +0200

    added TLS support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
+var forceSSL = require('express-force-ssl');
 var approvedBlock = require('./config/config');
 
 // Init the app
@@ -17,6 +18,7 @@ app.use(favicon(path.join(__dirname, '/public/images/favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(forceSSL);
 
 // Routes
 app.get('/', function(req, res) {
