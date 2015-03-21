a363db9e1779f459308585bf40aa0b2c73c1276a
cubedro <contact@siteshop.ro>
2015-03-21 22:14:34 +0200

    fixed view/hide source buttons

--- public/js/script.js
+++ public/js/script.js
@@ -3,15 +3,18 @@
 
 	$('#shell-source').hide().removeClass('hidden');
 	$('#install-info').hide().removeClass('hidden');
+	$('#hide-source').hide().removeClass('hidden');
 
 	$('#show-source').on('click', function() {
 		$('#shell-source').show();
+		$('#hide-source').show();
 		$(this).hide();
 	});
 
 	$('#hide-source').on('click', function() {
 		$('#show-source').show();
 		$('#shell-source').hide();
+		$(this).hide();
 	});
 
 	$('#understand').change(function() {
