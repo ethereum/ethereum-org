fecb573fae400b2504b67d8ff52c7b13f543c3ae
Lefteris Karapetsas <lefteris@refu.co>
2015-07-29 13:35:57 +0200

    Provide default value in Linux install prompt
    In most Unix scripts when you are queried with a [Y/n] it is
    always interpreted that the capitalized option is the default
    (chosen by simply pressing Enter).
    
    So we are adding Yes as the default option.

--- bin/install-cpp.sh
+++ bin/install-cpp.sh
@@ -95,9 +95,11 @@ function run_installer()
 	function wait_for_user() {
 		while :
 		do
-			read -p "${blue}==>${reset} $1 (Y/n) " imp
+			read -p "${blue}==>${reset} $1 [Y/n] " imp
 			case $imp in
 				[yY] ) echo; break ;;
+				'' ) echo; break ;;
+				[nN] ) abortInstall "${red}==>${reset} Process stopped by user. To resume the install run the one-liner command again." ;;
 				* ) abortInstall "${red}==>${reset} Process stopped by user. To resume the install run the one-liner command again." ;;
 			esac
 		done
