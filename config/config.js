b9c99ed499a78e4b2aa0b6fe8fcc6224ce517545
cubedro <contact@siteshop.ro>
2015-07-29 17:20:52 +0300

    clean-up unnecessary files

--- config/config.js
+++
@@ -1,7 +0,0 @@
-var approvedBlock = {
-	number: 146118,
-	hash: "dea5342d507a30c49e6298b10d44dc5e047a27260a284dc78f6c96781c6f6e58",
-	timestamp: Math.floor(Date.now()/1000)
-};
-
-module.exports = approvedBlock;
