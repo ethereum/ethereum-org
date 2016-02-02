375aa24decc8bf6baa24a33598929c97ab9a8344
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-02-02 19:45:45 -0200

    projects

--- public/js/script.js
+++ public/js/script.js
@@ -29,14 +29,48 @@ $(document).ready(function() {
           toc.classList.remove(className);
         else
           toc.className = toc.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-
-
       }
     }
-  }
-  
+  };
+
+
+
+  // Usage:
+  shuffle( document.getElementsByClassName('slide-link') );
+
 
 });
+
+
+// WARNING: I haven't yet tested this
+// - It's likely that it will strip events upon shuffling
+function shuffle(elems) {
+ 
+    allElems = (function(){
+  var ret = [], l = elems.length;
+  while (l--) { ret[ret.length] = elems[l]; }
+  return ret;
+    })();
+ 
+    var shuffled = (function(){
+        var l = allElems.length, ret = [];
+        while (l--) {
+            var random = Math.floor(Math.random() * allElems.length),
+                randEl = allElems[random].cloneNode(true);
+            allElems.splice(random, 1);
+            ret[ret.length] = randEl;
+        }
+        return ret; 
+    })(), l = elems.length;
+ 
+    while (l--) {
+        elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
+        elems[l].parentNode.removeChild(elems[l]);
+    }
+ 
+}
+ 
+
 
 
 /* 
