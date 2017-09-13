805a301d83adc3dd489de24e24758e05715bb0af
Alex Van de Sande <avsa@ethereum.org>
2017-09-13 18:57:03 -0300

    Revert "Update frontier references (#668)"
    This reverts commit b0b550373e4e9b24089c6d49ab626359d47ada98.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,8 +38,8 @@ module.exports = function(grunt) {
 		},
 		clean: {
 			build: ['dist'],
-			cleanup_js: ['dist/js/*.*', '!dist/js/app.*'],
-			cleanup_css: ['dist/css/*.css', '!dist/css/app.*.css']
+			cleanup_js: ['dist/js/*.*', '!dist/js/frontier.*'],
+			cleanup_css: ['dist/css/*.css', '!dist/css/frontier.*.css']
 		},
 		jade: {
 			build: {
@@ -198,16 +198,16 @@ module.exports = function(grunt) {
 				src: app,
 				dest: 'dist/js/app.js'
 			},
-			js: {
+			frontier: {
 				options: {
 					sourceMap: true
 				},
 				src: ['<%= uglify.vendor.dest %>', '<%= uglify.app.dest %>'],
-				dest: 'dist/js/app.min.js'
+				dest: 'dist/js/frontier.min.js'
 			},
 			css: {
 				src: ['dist/css/*.min.css', 'dist/css/*.css'],
-				dest: 'dist/css/app.min.css'
+				dest: 'dist/css/frontier.min.css'
 			}
 		},
 		uglify: {
@@ -233,6 +233,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-http');
 
-	grunt.registerTask('default', ['http', 'clean', 'jade', 'copy', 'cssmin', 'concat:vendor', 'concat:app', 'uglify', 'concat:js', 'concat:css', 'clean:cleanup_js', 'clean:cleanup_css']);
+	grunt.registerTask('default', ['http', 'clean', 'jade', 'copy', 'cssmin', 'concat:vendor', 'concat:app', 'uglify', 'concat:frontier', 'concat:css', 'clean:cleanup_js', 'clean:cleanup_css']);
 	grunt.registerTask('build', 'default');
 };
