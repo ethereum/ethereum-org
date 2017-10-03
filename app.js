5a6810efb4d13b55147a2162ffa5893ff2b63130
root <root@ip-172-31-16-245.ap-south-1.compute.internal>
2017-10-03 18:28:38 +0000

    es6 changes

--- app.js
+++ app.js
@@ -1,106 +1,91 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 
 // Init the app
-var app = express();
+const app = express();
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(favicon(path.join(__dirname, '/dist/images/favicon.png')));
+app.set('view engine', 'jade');
+//app.use(favicon(path.join(__dirname, '/dist/images/favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 
 // Routes
-app.get('/', function(req, res) {
-	res.render('index');
+app.get('/', (request, response) => {
+	response.render('index');
 });
 
-app.get('/wallet', function(req, res) {
-	res.render('wallet')
+app.get('/cli', (request, response) => {
+	response.render('cli');
 });
 
-app.get('/cli', function(req, res) {
-	res.render('cli');
+app.get('/ether', (request, response) => {
+	response.render('ether');
 });
 
-app.get('/ether', function(req, res) {
-	res.render('ether');
+app.get('/greeter', (request, response) => {
+	response.render('greeter');
 });
 
-app.get('/greeter', function(req, res) {
-	res.render('greeter');
+app.get('/token', (request, response) => {
+	response.render('token');
 });
 
-app.get('/token', function(req, res) {
-	res.render('token');
+app.get('/crowdsale', (request, response) => {
+	response.render('crowdsale');
 });
 
-app.get('/crowdsale', function(req, res) {
-	res.render('crowdsale');
+app.get('/dao', (request, response) => {
+	response.render('dao');
 });
 
-app.get('/dao', function(req, res) {
-	res.render('dao');
+app.get('/agreement', (request, response) => {
+	response.render('agreement');
 });
 
-app.get('/agreement', function(req, res) {
-	res.render('agreement');
+app.get('/assets', (request, response) => {
+	response.render('assets');
 });
 
-app.get('/assets', function(req, res) {
-	res.render('assets');
+app.get('/brand', (request, response) => {
+	response.render('brand');
 });
 
-app.get('/brand', function(req, res) {
-	res.render('brand');
+app.get('/foundation', (request, response) => {
+	response.render('foundation');
 });
 
-app.get('/foundation', function(req, res) {
-	res.render('foundation');
+app.get('/donate', (request, response) => {
+	response.render('donate');
 });
 
-app.get('/donate', function(req, res) {
-	res.render('donate');
+app.get('/devgrants', (request, response) => {
+	response.render('devgrants');
 });
 
-app.get('/devgrants', function(req, res) {
-	res.render('devgrants');
+app.get('/devcontwo', (request, response) => {
+	response.render('devcon2');
 });
 
-app.get('/devcontwo', function(req, res) {
-	res.render('devcon2');
+app.get('/swarm', (request, response) => {
+	response.render('swarm');
 });
 
-app.get('/swarm', function(req, res) {
-	res.render('swarm');
-});
-
-app.get('/privacy-policy', function(req, res) {
-	res.render('privacy-policy');
-});
-
-app.get('/cookie-policy', function(req, res) {
-	res.render('cookie-policy');
-});
-
-app.get('/terms-of-use', function(req, res) {
-	res.render('terms-of-use');
-});
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
+app.use((request, response, next) => {
+	let err = new Error('Not Found');
 	err.status = 404;
 	
-	// respond with html page
-	if (req.accepts('html')) {
-	    res.render('404', { url: req.url });
+	// responsepond with html page
+	if (request.accepts('html')) {
+	    response.render('404', { url: request.url });
 	    return;
 	}
 	
@@ -109,9 +94,9 @@ app.use(function(req, res, next) {
 
 // error handlers
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
+	app.use((err, request, response, next) => {
+		response.status(err.status || 500);
+		response.render('error', {
 			message: err.message,
 			error: err
 		});
@@ -119,9 +104,9 @@ if (app.get('env') === 'development') {
 }
 
 // production error handler
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
+app.use((err, request, response, next) => {
+	response.status(err.status || 500);
+	response.render('error', {
 		message: err.message,
 		error: {}
 	});
