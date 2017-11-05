379c4b77f871c8a20535091999d0e84864f10b52
Scott Satkin <scott@satkin.com>
2017-11-04 22:54:14 -0700

    Fix bug preventing last token from being transferred

--- solidity/token-advanced.sol
+++ solidity/token-advanced.sol
@@ -193,7 +193,7 @@ contract MyAdvancedToken is owned, TokenERC20 {
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) internal {
         require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] > _value);                // Check if the sender has enough
+        require (balanceOf[_from] >= _value);               // Check if the sender has enough
         require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         require(!frozenAccount[_from]);                     // Check if sender is frozen
         require(!frozenAccount[_to]);                       // Check if recipient is frozen
