802645a5f3df0d2cc7dae89e833c1142a075f22b
p0n1 <36690236+p0n1@users.noreply.github.com>
2018-06-08 09:39:47 +0800

    token-minimal.sol: compatible erc20
    Same as https://github.com/ethereum/ethereum-org/pull/859.

--- solidity/token-minimal.sol
+++ solidity/token-minimal.sol
@@ -12,10 +12,11 @@ contract MyToken {
     }
 
     /* Send coins */
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
         require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
         balanceOf[msg.sender] -= _value;                    // Subtract from the sender
         balanceOf[_to] += _value;                           // Add the same to the recipient
+        return true;
     }
 }
