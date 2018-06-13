db9f29781ec3bc3a574069302a7e8d0871e02ded
Everton Fraga <ev@ethereum.org>
2018-06-12 21:17:47 -0300

    Code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,231 +1,236 @@
 var src = 'public/';
 var dest = 'dist/';
 
-var app = [
-	'public/js/ether-checker.js',
-	'public/js/script.js'
-];
+var app = ['public/js/ether-checker.js', 'public/js/script.js'];
 
 var vendor = [
-	'public/js/jquery-1.11.2.min.js',
-	'public/js/bootstrap.min.js',
-	'public/js/jquery.scrollme.min.js',
-	'public/js/bitcoinjs-min.js',
-	'public/js/numeral.js',
-	'public/js/jquery.tableofcontents.min.js'
+  'public/js/jquery-1.11.2.min.js',
+  'public/js/bootstrap.min.js',
+  'public/js/jquery.scrollme.min.js',
+  'public/js/bitcoinjs-min.js',
+  'public/js/numeral.js',
+  'public/js/jquery.tableofcontents.min.js'
 ];
 
-var styles = [
-	'bootstrap.min.css',
-	'font-awesome.min.css',
-	'style.css'
-];
+var styles = ['bootstrap.min.css', 'font-awesome.min.css', 'style.css'];
 
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		http: {
-			fetch_mist_releases: {
-				options: {
-					headers: {
-						'User-Agent': 'Ethereum.org-Gruntfile'
-					},
-					url: 'https://api.github.com/repos/ethereum/mist/releases'
-				},
-				dest: 'data/mist_releases.json'
-			}
-		},
-		clean: {
-			build: ['dist'],
-			cleanup_js: ['dist/js/*.*', '!dist/js/app.*'],
-			cleanup_css: ['dist/css/*.css', '!dist/css/app.*.css']
-		},
-		pug: {
-			build: {
-				options: {
-					data: function(dest, src) {
-						var mistReleases = grunt.file.readJSON("data/mist_releases.json")[0]['assets'].filter(e => /Ethereum-Wallet/.test(e.name));
-						var mistReleaseOSX,
-							mistReleaseWin64,
-							mistReleaseWin32;
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    http: {
+      fetch_mist_releases: {
+        options: {
+          headers: {
+            'User-Agent': 'Ethereum.org-Gruntfile'
+          },
+          url: 'https://api.github.com/repos/ethereum/mist/releases'
+        },
+        dest: 'data/mist_releases.json'
+      }
+    },
+    clean: {
+      build: ['dist'],
+      cleanup_js: ['dist/js/*.*', '!dist/js/app.*'],
+      cleanup_css: ['dist/css/*.css', '!dist/css/app.*.css']
+    },
+    pug: {
+      build: {
+        options: {
+          data: function(dest, src) {
+            var mistReleases = grunt.file
+              .readJSON('data/mist_releases.json')[0]
+              ['assets'].filter(e => /Ethereum-Wallet/.test(e.name));
+            var mistReleaseOSX, mistReleaseWin64, mistReleaseWin32;
 
-						for (var i = 0; i < mistReleases.length; i++) {
-							var obj = mistReleases[i];
+            for (var i = 0; i < mistReleases.length; i++) {
+              var obj = mistReleases[i];
 
-							if (obj['name'].indexOf('macosx') !== -1) {
-								mistReleaseOSX = obj['browser_download_url'];
-							} else if (obj['name'].indexOf('win64') !== -1) {
-								mistReleaseWin64 = obj['browser_download_url'];
-							} else if (obj['name'].indexOf('win32') !== -1) {
-								mistReleaseWin32 = obj['browser_download_url'];
-							}
-						}
+              if (obj['name'].indexOf('macosx') !== -1) {
+                mistReleaseOSX = obj['browser_download_url'];
+              } else if (obj['name'].indexOf('win64') !== -1) {
+                mistReleaseWin64 = obj['browser_download_url'];
+              } else if (obj['name'].indexOf('win32') !== -1) {
+                mistReleaseWin32 = obj['browser_download_url'];
+              }
+            }
 
-						return {
-							debug: false,
-							pretty: false,
-							block: {
-								hash: '<%= pkg.hash %>'
-							},
-							mistReleaseOSX: mistReleaseOSX,
-							mistReleaseWin64: mistReleaseWin64,
-							mistReleaseWin32: mistReleaseWin32
-						};
-					}
-				},
-				files: [
-					{
-						'dist/index.html': 'views/index.pug'
-					},
-					{
-						'dist/agreement.html': 'views/agreement.pug'
-					},
-					{
-						'dist/crowdsale.html': 'views/crowdsale.pug'
-					},
-					{
-						'dist/dao.html': 'views/dao.pug'
-					},
-					{
-						'dist/ether.html': 'views/ether.pug'
-					},
-					{
-						'dist/cli.html': 'views/cli.pug'
-					},
-					{
-						'dist/greeter.html': 'views/greeter.pug'
-					},
-					{
-						'dist/assets.html': 'views/assets.pug'
-					},
-					{
-						'dist/sale.html': 'views/sale.pug'
-					},
-					{
-						'dist/token.html': 'views/token.pug'
-					},
-					{
-						'dist/brand.html': 'views/brand.pug'
-					},
-					{
-						'dist/foundation.html': 'views/foundation.pug'
-					},
-					{
-						'dist/donate.html': 'views/donate.pug'
-					},
-					// {
-					// 	'dist/swarm.html': 'views/swarm.pug'
-					// },
-					{
-						'dist/devgrants.html': 'views/devgrants.pug'
-					},
-					{
-						'dist/privacy-policy.html': 'views/privacy-policy.pug'
-					},
-					{
-						'dist/cookie-policy.html': 'views/cookie-policy.pug'
-					},
-					{
-						'dist/terms-of-use.html': 'views/terms-of-use.pug'
-					}
-				]
-			}
-		},
-		copy: {
-			build: {
-				files: [
-					{
-						expand: true,
-						cwd: 'public/fonts/',
-						src: ['*.*'],
-						dest: 'dist/fonts/',
-						filter: 'isFile'
-					},
-					{
-						expand: true,
-						cwd: 'public/images/',
-						src: ['*.ico'],
-						dest: 'dist/',
-						filter: 'isFile'
-					},
-					{
-						expand: true,
-						cwd: 'public/images/',
-						src: ['**/*.*'],
-						dest: 'dist/images/',
-						filter: 'isFile'
-					},
-					{
-						expand: true,
-						cwd: 'public/css/',
-						src: styles,
-						dest: 'dist/css/',
-						filter: 'isFile'
-					},
-					{
-						src: 'public/js/jquery-1.11.2.min.map',
-						dest: 'dist/js/jquery-1.11.2.min.map'
-					}
-				]
-			}
-		},
-		cssmin: {
-			build: {
-				files: [{
-					expand: true,
-					cwd: 'dist/css',
-					src: ['*.css', '!*.min.css'],
-					dest: 'dist/css/'
-				}]
-			}
-		},
-		concat: {
-			vendor: {
-				src: vendor,
-				dest: 'dist/js/vendor.js'
-			},
-			app : {
-				options: {
-					separator: ';',
-				},
-				src: app,
-				dest: 'dist/js/app.js'
-			},
-			js: {
-				options: {
-					sourceMap: true
-				},
-				src: ['<%= uglify.vendor.dest %>', '<%= uglify.app.dest %>'],
-				dest: 'dist/js/app.min.js'
-			},
-			css: {
-				src: ['dist/css/*.min.css', 'dist/css/*.css'],
-				dest: 'dist/css/app.min.css'
-			}
-		},
-		uglify: {
-			app: {
-				dest: 'dist/js/app.min.js',
-				src: ['<%= concat.app.dest %>']
-			},
-			vendor: {
-				dest: 'dist/js/vendor.min.js',
-				src: ['<%= concat.vendor.dest %>']
-			},
-			options: {
-				mangle: false
-			}
-		}
-	});
+            return {
+              debug: false,
+              pretty: false,
+              block: {
+                hash: '<%= pkg.hash %>'
+              },
+              mistReleaseOSX: mistReleaseOSX,
+              mistReleaseWin64: mistReleaseWin64,
+              mistReleaseWin32: mistReleaseWin32
+            };
+          }
+        },
+        files: [
+          {
+            'dist/index.html': 'views/index.pug'
+          },
+          {
+            'dist/agreement.html': 'views/agreement.pug'
+          },
+          {
+            'dist/crowdsale.html': 'views/crowdsale.pug'
+          },
+          {
+            'dist/dao.html': 'views/dao.pug'
+          },
+          {
+            'dist/ether.html': 'views/ether.pug'
+          },
+          {
+            'dist/cli.html': 'views/cli.pug'
+          },
+          {
+            'dist/greeter.html': 'views/greeter.pug'
+          },
+          {
+            'dist/assets.html': 'views/assets.pug'
+          },
+          {
+            'dist/sale.html': 'views/sale.pug'
+          },
+          {
+            'dist/token.html': 'views/token.pug'
+          },
+          {
+            'dist/brand.html': 'views/brand.pug'
+          },
+          {
+            'dist/foundation.html': 'views/foundation.pug'
+          },
+          {
+            'dist/donate.html': 'views/donate.pug'
+          },
+          {
+            'dist/devgrants.html': 'views/devgrants.pug'
+          },
+          {
+            'dist/privacy-policy.html': 'views/privacy-policy.pug'
+          },
+          {
+            'dist/cookie-policy.html': 'views/cookie-policy.pug'
+          },
+          {
+            'dist/terms-of-use.html': 'views/terms-of-use.pug'
+          }
+        ]
+      }
+    },
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'public/fonts/',
+            src: ['*.*'],
+            dest: 'dist/fonts/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'public/images/',
+            src: ['*.ico'],
+            dest: 'dist/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'public/images/',
+            src: ['**/*.*'],
+            dest: 'dist/images/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'public/css/',
+            src: styles,
+            dest: 'dist/css/',
+            filter: 'isFile'
+          },
+          {
+            src: 'public/js/jquery-1.11.2.min.map',
+            dest: 'dist/js/jquery-1.11.2.min.map'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'dist/css/'
+          }
+        ]
+      }
+    },
+    concat: {
+      vendor: {
+        src: vendor,
+        dest: 'dist/js/vendor.js'
+      },
+      app: {
+        options: {
+          separator: ';'
+        },
+        src: app,
+        dest: 'dist/js/app.js'
+      },
+      js: {
+        options: {
+          sourceMap: true
+        },
+        src: ['<%= uglify.vendor.dest %>', '<%= uglify.app.dest %>'],
+        dest: 'dist/js/app.min.js'
+      },
+      css: {
+        src: ['dist/css/*.min.css', 'dist/css/*.css'],
+        dest: 'dist/css/app.min.css'
+      }
+    },
+    uglify: {
+      app: {
+        dest: 'dist/js/app.min.js',
+        src: ['<%= concat.app.dest %>']
+      },
+      vendor: {
+        dest: 'dist/js/vendor.min.js',
+        src: ['<%= concat.vendor.dest %>']
+      },
+      options: {
+        mangle: false
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-pug');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-http');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-http');
 
-	grunt.registerTask('default', ['http', 'clean', 'pug', 'copy', 'cssmin', 'concat:vendor', 'concat:app', 'uglify', 'concat:js', 'concat:css', 'clean:cleanup_js', 'clean:cleanup_css']);
-	grunt.registerTask('build', 'default');
+  grunt.registerTask('default', [
+    'http',
+    'clean',
+    'pug',
+    'copy',
+    'cssmin',
+    'concat:vendor',
+    'concat:app',
+    'uglify',
+    'concat:js',
+    'concat:css',
+    'clean:cleanup_js',
+    'clean:cleanup_css'
+  ]);
+  grunt.registerTask('build', 'default');
 };
--- app.js
+++ app.js
@@ -19,104 +19,103 @@ app.set('port', process.env.PORT || 8080);
 
 // Routes
 app.get('/', (request, response) => {
-	response.render('index');
+  response.render('index');
 });
 
 app.get('/wallet', (request, response) => {
-	response.render('wallet')
+  response.render('wallet');
 });
 
 app.get('/cli', (request, response) => {
-	response.render('cli');
+  response.render('cli');
 });
 
 app.get('/ether', (request, response) => {
-	response.render('ether');
+  response.render('ether');
 });
 
 app.get('/greeter', (request, response) => {
-	response.render('greeter');
+  response.render('greeter');
 });
 
 app.get('/token', (request, response) => {
-	response.render('token');
+  response.render('token');
 });
 
 app.get('/crowdsale', (request, response) => {
-	response.render('crowdsale');
+  response.render('crowdsale');
 });
 
 app.get('/dao', (request, response) => {
-	response.render('dao');
+  response.render('dao');
 });
 
 app.get('/agreement', (request, response) => {
-	response.render('agreement');
+  response.render('agreement');
 });
 
 app.get('/assets', (request, response) => {
-	response.render('assets');
+  response.render('assets');
 });
 
 app.get('/brand', (request, response) => {
-	response.render('brand');
+  response.render('brand');
 });
 
 app.get('/foundation', (request, response) => {
-	response.render('foundation');
+  response.render('foundation');
 });
 
 app.get('/donate', (request, response) => {
-	response.render('donate');
+  response.render('donate');
 });
 
 app.get('/devgrants', (request, response) => {
-	response.render('devgrants');
+  response.render('devgrants');
 });
 
 app.get('/devcontwo', (request, response) => {
-	response.render('devcon2');
+  response.render('devcon2');
 });
 
 app.get('/swarm', (request, response) => {
-	response.render('swarm');
+  response.render('swarm');
 });
 
-app.get('/privacy-policy',(request, response) => {
-	response.render('privacy-policy');
+app.get('/privacy-policy', (request, response) => {
+  response.render('privacy-policy');
 });
 
-app.get('/cookie-policy',(request, response) => {
-	response.render('cookie-policy');
+app.get('/cookie-policy', (request, response) => {
+  response.render('cookie-policy');
 });
 
 app.get('/terms-of-use', (request, response) => {
-	response.render('terms-of-use');
+  response.render('terms-of-use');
 });
 
-app.get('/*', (request,response,next) => {
-		let errStatus = 404;
+app.get('/*', (request, response, next) => {
+  let errStatus = 404;
 
-		// respond with html page
-		if (request.accepts('html')) {
-				response.render('404', { url: request.url });
-				return;
-		}else{
-			 response.status(errStatus).end();
-		}
-
+  // respond with html page
+  if (request.accepts('html')) {
+    response.render('404', { url: request.url });
+    return;
+  } else {
+    response.status(errStatus).end();
+  }
 });
 
 // error handlers
 if (app.get('env') === 'development') {
-	// development error handler(Not needed for now)
-	app.use((err, request, response, next) => {
-		response.status(err.status || 500);
-		response.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+  // development error handler(Not needed for now)
+  app.use((err, request, response, next) => {
+    response.status(err.status || 500);
+    response.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
 module.exports = app;
